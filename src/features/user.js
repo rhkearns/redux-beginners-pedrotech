895bf5e5549b139e305bf7dcc710e43fb32fd6ee
rhkearns <ryankearns217@gmail.com>
2022-01-19 15:51:06 -0500

    create userSlice

---
+++ src/features/user.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+	name: 'user',
+	initialState: {
+		value: {
+			name: '',
+			age: 0,
+			email: '',
+		},
+	},
+	reducer: {
+		login: (state, action) => {
+			state.value = action.payload;
+		},
+	},
+});
+
+export default userSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/user'
+
+const store = configureStore({
+	reducer: {
+    user: userReducer,
+  },
+});
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
