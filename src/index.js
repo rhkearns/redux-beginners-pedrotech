f05f6793cc2c78e2c109648835bf2ded7f5ddc6b
rhkearns <ryankearns217@gmail.com>
2022-01-19 16:15:54 -0500

    create theme reducer

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user'
+import themeReducer from './features/theme'
 
 const store = configureStore({
 	reducer: {
     user: userReducer,
+		theme: themeReducer,
   },
 });
 
