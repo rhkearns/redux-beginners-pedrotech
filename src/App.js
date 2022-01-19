3c67233f4d88ed67de0e5fa4a8ed5e59f29d6bee
rhkearns <ryankearns217@gmail.com>
2022-01-19 15:38:50 -0500

    import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Profile />
+      <Login />
     </div>
   );
 }
